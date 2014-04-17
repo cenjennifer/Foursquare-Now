797d2d4667721abea6756c2efccfb72502943fc9
Jennifer C <cenjennifer@gmail.com>
2014-04-16 20:09:28 -0400

    Draft 1
    API HACK few steps

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready( function() {
+	$('.unanswered-getter').submit( function(event){
+		// zero out results if previous search has run
+		$('.results').html('');
+		// get the value of the tags the user submitted
+		var tags = $(this).find("input[name='locations']").val();
+		getTopCheckin(tags);
+	});
+});
